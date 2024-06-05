d927cf958f6bbab8755f2a9a3d495786f2a99cf7
Ikhsan-Fatahillah <ikhsanngkalu@gmail.com>
2024-06-06 02:45:40 +0700

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import Akun from "./models/UserModel2.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: ['http://localhost:3000', "https://manajemen-frontend-cvbl6cy73a-et.a.run.app"] }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(UserRoute);
@@ -22,6 +22,6 @@ try {
 }
 
 // Gunakan port dari variabel lingkungan atau default ke 8080
-const port = 5000 || 8080;
+const port = 8080;
 
 app.listen(port, () => console.log(`Server up and running on port ${port}...`));
