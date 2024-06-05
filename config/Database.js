fc26ce52cc055879b162dedafea43b44e429acf6
Ikhsan-Fatahillah <ikhsanngkalu@gmail.com>
2024-06-06 03:09:15 +0700

    fix

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('crud_db', 'root', '', {
+const db = new Sequelize('crud_db', 'root', 'u~1rBp_{U5/:1=M,', {
     host: '104.198.138.95',
     dialect: 'mysql'
 });
