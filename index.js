4d88aacc1741117c7429b254bf90b55e5855d97d
Ikhsan-Fatahillah <ikhsanngkalu@gmail.com>
2024-06-06 03:30:51 +0700

    fix

--- index.js
+++ index.js
@@ -24,14 +24,14 @@ app.use(cors(
 app.use(cookieParser());
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// })
 
 
 
